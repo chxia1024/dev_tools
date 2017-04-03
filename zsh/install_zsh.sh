38fc7a6036094cdb208422e0f3f6cf4d49940428
chxia <chxia1024@gmail.com>
2017-04-03 15:07:40 +0800

    use zsh version 5.3 to fix the bug of command line messed up by PASTE
    Change-Id: I94e0a3a19595d740a931393c3b436b5d550a9ab4

--- zsh/install_zsh.sh
+++ zsh/install_zsh.sh
@@ -11,6 +11,8 @@ zsh_git="git@github.com:zsh-users/zsh.git"
 dir="$CHXIA/sw/zsh/"
 clone_git_repos $dir $zsh_git
 cd $dir/zsh
+# checkout branch 5.3 to fix the bug of command line messed up by PASTE
+git checkout zsh-5.3 -b zsh-5.3
 ./Util/preconfig
 check_ret $? "preconfig zsh"
 ./configure --prefix=$ZSH_INSTALL --enable-zsh-debug
